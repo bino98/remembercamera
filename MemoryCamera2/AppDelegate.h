2990bfa3b0633d007f416f57b37766cd0d25b3f3
Takafumi Yamamoto <bino98@gmail.com>
2013-11-12 17:15:58 +0900

    Initial Commit

---
+++ MemoryCamera2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryCamera2
+//
+//  Created by Takafumi Yamamoto on 2013/11/12.
+//  Copyright (c) 2013年 Takafumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
